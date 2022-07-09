2d8ce5376a342626905a9f0c3df3fdef7b3f29c9
Venkanna77 <venkanna948@gmail.com>
2022-07-09 07:37:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-jggd3c
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-jggd3c)
---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import './style.css';
+
+const country_list = [
+  'Argentina',
+  'Australia',
+  'Belgium',
+  'Belize',
+  'China',
+  'Iceland',
+  'India',
+  'Indonesia',
+  'Iran',
+  'Poland',
+  'Portugal',
+  'Romania',
+  'Russia',
+  'Saudi Arabia',
+  'South Africa',
+  'South Korea',
+  'Swaziland',
+  'Sweden',
+  'Switzerland',
+];
+
+export default function App() {
+  const [result, setResult] = useState([...country_list]);
+
+  const searchList = (e) => {
+    let a = e.target.value;
+    let new_list = country_list.filter((i) => {
+      if (a == '') return i;
+      else return i.toLocaleLowerCase().includes(a);
+    });
+    setResult(new_list);
+  };
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+      <input type="text" onChange={(e) => searchList(e)} />
+      {result.map((i) => (
+        <ul key={i}>
+          <li>{i}</li>
+        </ul>
+      ))}
+    </div>
+  );
+}
